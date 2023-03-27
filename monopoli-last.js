1242e3a28ea9ffdc2dec0aaaed72fcb79b2de73a
DemianDoru <dorutz_ionutz@yahoo.com>
2023-03-27 19:58:58 +0300

    some modiffications

---
+++ monopoli-last.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//Selecting Elements
+const player1 = document.querySelector('.player1');
+const player2 = document.querySelector('.player2');
+const scorePlayer1 = document.querySelector('#current-position-1');
+const scorePlayer2 = document.querySelector('#current-position-2');
+const currentDice1 = document.getElementById('current-dice-1');
+const currentDice2 = document.getElementById('current-dice-2');
+
+const diceElem = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn-roll');
+const btnNew = document.querySelector('.btn-new');
+const btnMove = document.querySelector('.btn-move');
