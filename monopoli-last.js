206e545952751ab56dd4ff9d6af8b53b65f710f0
DemianDoru <dorutz_ionutz@yahoo.com>
2023-03-28 08:53:05 +0300

    some other modif

--- monopoli-last.js
+++ monopoli-last.js
@@ -1,14 +1,36 @@
 'use strict';
 
+// const player1 = document.querySelector('.player1');
+// const player2 = document.querySelector('.player2');
+
 //Selecting Elements
-const player1 = document.querySelector('.player1');
-const player2 = document.querySelector('.player2');
-const scorePlayer1 = document.querySelector('#current-position-1');
-const scorePlayer2 = document.querySelector('#current-position-2');
+let scorePlayer1 = document.querySelector('#current-position-1');
+let scorePlayer2 = document.querySelector('#current-position-2');
+const diceElem = document.querySelector('.dice');
 const currentDice1 = document.getElementById('current-dice-1');
 const currentDice2 = document.getElementById('current-dice-2');
 
-const diceElem = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn-roll');
 const btnNew = document.querySelector('.btn-new');
 const btnMove = document.querySelector('.btn-move');
+
+//Starting conditions
+scorePlayer1 = 0;
+scorePlayer2 = 0;
+diceElem.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+//Rolling Dice
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a dice roll
+  let dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  // 2. Display Dice
+  diceElem.classList.remove('hidden');
+  diceElem.src = `dice-${dice}.png`;
+  currentScore += dice;
+  currentDice1.textContent = currentScore;
+});
